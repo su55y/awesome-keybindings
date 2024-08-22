208e1ce628690c2c845375a38110178ec152555c
su55y <cesarmilano88@yahoo.com>
2024-08-22 23:11:57 +0300

    links fix

--- utils/raw_to_md.py
+++ utils/raw_to_md.py
@@ -1,1 +1,35 @@
-/home/commander/code/git_su55y/keybinds/raw_to_md.py
+import argparse
+from pathlib import Path
+
+
+def parse_args() -> tuple[Path, Path, str]:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("source", type=Path, help="Source file")
+    parser.add_argument("-d", "--dest", type=Path, help="Dest file")
+    parser.add_argument("-u", "--url", help="Credits url (raw data source link)")
+    args = parser.parse_args()
+    return args.source, args.dest, args.url
+
+
+if __name__ == "__main__":
+    source, dest, src_url = parse_args()
+    if not source.exists():
+        print(f"source file '{source}' not exists")
+        exit(1)
+    if not source.is_file():
+        print(f"source file '{source}' is not file")
+        exit(1)
+    if dest is None:
+        dest = source.with_name(f"{source.name}.md")
+
+    source_reader = (row for row in open(source))
+    md_lines = ["| _key_ | description |\n", "| :---: | :----------------: |\n"]
+    if src_url:
+        md_lines.insert(0, f"[source]({src_url})\n")
+
+    for row in source_reader:
+        key, desc = row.split("\000")
+        md_lines.append(f"| {key.strip()} | {desc.strip()} |\n")
+
+    with open(dest, "w") as dest_fd:
+        dest_fd.writelines(md_lines)
--- utils/rofi-launcher/helper.sh
+++ utils/rofi-launcher/helper.sh
@@ -1,1 +1,32 @@
-/home/commander/code/git_su55y/keybinds/helper.sh
+#!/bin/sh
+
+REPO_ROOT_PATH="$(
+	cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit 1
+	pwd -P
+)"
+
+printf "\000markup-rows\037true\n"
+
+print_main() {
+	printf '\000data\037main\n'
+	find "$REPO_ROOT_PATH" -name raw | while read -r line; do
+		raw_path="$line"
+		raw_name="$(basename "$(dirname "$line")")"
+		printf '%s\000info\037%s\n' "$raw_name" "$raw_path"
+	done
+}
+
+print_raw() {
+	printf '\000data\037raw\n'
+	awk -F '\000' '{printf "<b>%s</b> <i>%s</i>\n", $1, $2}' "$ROFI_INFO"
+}
+
+case $ROFI_RETV in
+1)
+	case $ROFI_DATA in
+	main) print_raw "$ROFI_INFO" ;;
+	raw) ;;
+	esac
+	;;
+*) print_main ;;
+esac
